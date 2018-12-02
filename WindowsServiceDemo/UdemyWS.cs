79463b90b581915b50c007bd957faf08b9178a3e
yusufmsalh <yusufm.salh@gmail.com>
2018-12-02 11:41:31 +0200

    ..

--- WindowsServiceDemo/UdemyWS.cs
+++ WindowsServiceDemo/UdemyWS.cs
@@ -1,19 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Linq;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 using log4net;
+using System.Threading;
+using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace WindowsServiceDemo
 {
     public partial class UdemyWS : ServiceBase
     {
         ILog mLogger;
+        private Timer mRepeatingTimer;
+        private double mCounter;
+        private FileSystemWatcher fileSystemWatcher;
         public UdemyWS()
         {
             InitializeComponent();
@@ -33,6 +33,16 @@ namespace WindowsServiceDemo
                 {
                     System.Diagnostics.Debug.WriteLine(string.Format("Value of {0} is at {1} ", i, DateTime.Now.ToShortDateString()));
                 }
+                //mRepeatingTimer = new Timer(myTimerCallback, mRepeatingTimer, 1000, 1000);
+                fileSystemWatcher = new FileSystemWatcher(@"C: \Users\Yusuf M.Salh\Documents\visual studio 2015\Projects\WindowsServiceDemo\WindowsServiceDemo\bin\Debug\WatchMe");
+                fileSystemWatcher.EnableRaisingEvents = true;
+                fileSystemWatcher.IncludeSubdirectories = true;
+                //when ever a file is x,x: created, call method written below
+                fileSystemWatcher.Created += FileSystemWatcherSomethingChangedInFolder;
+                fileSystemWatcher.Changed += FileSystemWatcherSomethingChangedInFolder;
+                fileSystemWatcher.Deleted += FileSystemWatcherSomethingChangedInFolder;
+                fileSystemWatcher.Renamed += FileSystemWatcherSomethingChangedInFolder;
+
             }
             catch (Exception)
             {
@@ -42,14 +52,22 @@ namespace WindowsServiceDemo
 
         }
 
+        private void FileSystemWatcherSomethingChangedInFolder(object sender, FileSystemEventArgs e)
+        {
+mLogger.Debug(string.Format("\r\n {0},\r\n {1}",e.ChangeType , e.FullPath));
+
+
+        }
+
         protected override void OnStop()
         {
             System.Diagnostics.Debugger.Launch();//Lanuching Debugger,make sure to stop service before this line,no need 
 
             ConfigureLog4Net();
             mLogger.Debug(string.Format("Hello World from Log4net at {0}", DateTime.Now.ToLongDateString()));
-            //   another example
             mLogger.Error("Nice Error");
+
+
 
         }
         private void ConfigureLog4Net()
@@ -64,6 +82,10 @@ namespace WindowsServiceDemo
                 EventLog.WriteEntry(ex.Message, EventLogEntryType.Error);
             }
         }
-
+        public void myTimerCallback(object objParam)
+        {
+            mLogger.Debug(string.Format("Value of counter is: {0}", mCounter++));
+            System.Diagnostics.Debug.WriteLine(string.Format("Value of counter is: {0}", mCounter));
+        }
     }
 }
