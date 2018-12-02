9f15f971ef62550e5c710a0e9f79e604741ceb07
yusufmsalh <yusufm.salh@gmail.com>
2018-12-02 10:03:40 +0200

    Adding Log4net Configurations.

--- WindowsServiceDemo/UdemyWS.cs
+++ WindowsServiceDemo/UdemyWS.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
 
 namespace WindowsServiceDemo
 {
     public partial class UdemyWS : ServiceBase
     {
+        ILog mLogger;
         public UdemyWS()
         {
             InitializeComponent();
@@ -19,13 +21,49 @@ namespace WindowsServiceDemo
 
         protected override void OnStart(string[] args)
         {
-            EventLog.WriteEntry("Hello World,udemy Windows Service Started",EventLogEntryType.Information);
+            try
+            {
+                EventLog.WriteEntry("Hello World,udemy Windows Service Started", EventLogEntryType.Information);
+                System.Diagnostics.Debugger.Launch();//Lanuching Debugger,make sure to stop service before this line,no need 
+                                                     //to install it again,already installed
+                ConfigureLog4Net();
+                 mLogger.Debug(string.Format("Hello World from Log4net at {0}", DateTime.Now.ToLongDateString()));
+
+                for (int i = 0; i < 5; i++)
+                {
+                    System.Diagnostics.Debug.WriteLine(string.Format("Value of {0} is at {1} ", i, DateTime.Now.ToShortDateString()));
+                }
+            }
+            catch (Exception)
+            {
+
+                //logging
+            }
+
         }
 
         protected override void OnStop()
         {
-            EventLog.WriteEntry("Hello World,udemy Windows Service  Stopped", EventLogEntryType.Information);
+            System.Diagnostics.Debugger.Launch();//Lanuching Debugger,make sure to stop service before this line,no need 
+
+            ConfigureLog4Net();
+            mLogger.Debug(string.Format("Hello World from Log4net at {0}", DateTime.Now.ToLongDateString()));
+            //   another example
+            mLogger.Error("Nice Error");
 
         }
+        private void ConfigureLog4Net()
+        {
+            try
+            {
+                log4net.Config.XmlConfigurator.Configure();
+                mLogger = LogManager.GetLogger("servicelog");
+            }
+            catch (Exception ex)
+            {
+                EventLog.WriteEntry(ex.Message, EventLogEntryType.Error);
+            }
+        }
+
     }
 }
